97650de80c7e51942d00af8c319297d3d73a8727
oerkperl <52177227+oerkperl@users.noreply.github.com>
2024-04-02 17:24:57 +0100

    more styling ref

--- app/components/portfolio/ProfileCard.tsx
+++ app/components/portfolio/ProfileCard.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Image from "next/image";
 import { useCryptoContext } from "@/app/context/context";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import Link from "next/link";
 
 export const ProfileCard: React.FC<{ myCoin: any }> = ({ myCoin }) => {
-  const { removeAsset, viewingCoinId, setViewingCoinId } = useCryptoContext();
+  const { setViewingCoinId } = useCryptoContext();
 
   return (
     <Link
       onClick={() => setViewingCoinId(myCoin?.id)}
       href={`/coin?id=${myCoin?.id}`}
-      className="w-1/4 flex  flex-col rounded-xl gap-4 justify-center items-center bg-gray-200 dark:bg-input-bg"
+      className="w-1/4 flex flex-col rounded-xl gap-4 justify-center items-center bg-gray-200 dark:bg-input-bg"
     >
       <div className="h-16 w-16 bg-gray-100 dark:bg-accent-bg rounded-md flex items-center justify-center ">
         <Image src={myCoin?.image} height={40} width={40} alt="coin image" />
